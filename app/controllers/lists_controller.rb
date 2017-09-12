056b55dc7268a526d486d6c9e5c26efcdd010fd8
ChristopherMaierInc <mail@christophermaier.com.au>
2017-09-12 14:36:41 +1000

    Pull this information out in the /lists/shopping view

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,4 +1,11 @@
 class ListsController < ApplicationController
+
   def shopping
+    @groceries = {
+	"Bread" => 2,
+	"Milk" => 2,
+  "Coke 1lt" => 2,
+  "Tracy's green juice" => 24,
+}
   end
 end
